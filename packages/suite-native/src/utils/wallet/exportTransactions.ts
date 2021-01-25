67daa56dff521482e3c79652c0c2f809e6700ef6
slowbackspace <slowbackspace@gmail.com>
2021-01-25 10:19:37 +0100

    fixup! chore:(suite-native): dummy file export in RN, fix `@suite-support` resolver path

--- packages/suite-native/src/utils/wallet/exportTransactions.ts
+++ packages/suite-native/src/utils/wallet/exportTransactions.ts
@@ -12,6 +12,6 @@ const preparePdf = (_fields: any, _content: any[], _coin: any) => {
     return {};
 };
 
-export const formatData = (_data: any) => {
-    return Promise.resolve();
+export const formatData = (_data: any): Promise<Blob> => {
+    return Promise.resolve(new Blob());
 };
